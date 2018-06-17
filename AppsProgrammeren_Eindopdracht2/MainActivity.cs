1ac7bbb711ca923b91e789d365247bed8924eb36
Jesse Jongsma <jessejongsma@hotmail.com>
2018-06-17 13:57:35 +0200

    initial commit

---
+++ AppsProgrammeren_Eindopdracht2/MainActivity.cs
@@ -0,0 +1,90 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Locations;
+using Android.Runtime;
+using Android.Content;
+using Android.Util;
+using System;
+
+namespace AppsProgrammeren_Eindopdracht2
+{
+    [Activity(Label = "AppsProgrammeren_Eindopdracht2", MainLauncher = true)]
+    public class MainActivity : Activity, ILocationListener
+    {
+        private PublicTransport _publicTransport;
+        private LocationManager _locationManager;
+        private double lat, lon;
+        private string _coordinates = null;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+            _locationManager = (LocationManager)GetSystemService(Context.LocationService);
+            _locationManager.RequestLocationUpdates(LocationManager.GpsProvider, 2000, 1, this);
+
+            Button button = FindViewById<Button>(Resource.Id.buttonSearch);
+
+            //Assign The Event To Button
+            button.Click += delegate
+            {
+
+                //Call Your Method When User Clicks The Button
+                clickButtonSearch();
+            };
+        }
+
+        public void OnLocationChanged(Location location)
+        {
+            // Show toast of location
+            lat = location.Latitude;
+            lon = location.Longitude;
+            _coordinates = lat.ToString() + ", " + lon.ToString();
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+            Toast.MakeText(this, "Please enable GPS.", ToastLength.Short).Show();
+        }
+
+        public void OnProviderEnabled(string provider) { }
+
+        public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
+        {
+            if (status == Availability.TemporarilyUnavailable || status == Availability.OutOfService)
+            {
+                Toast.MakeText(this, "Please check your GPS settings.", ToastLength.Short).Show();
+            }
+        }
+
+        public void clickButtonSearch()
+        {
+            string locationText = FindViewById<EditText>(Resource.Id.location).Text;
+            ListView listView = (ListView)FindViewById(Resource.Id.listView1);
+            try
+            {
+                _publicTransport = new PublicTransport(_coordinates, locationText);
+                ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, _publicTransport.GetTransitDetails());
+                listView.Adapter = adapter;
+            }
+            catch (Exception e)
+            {
+                // 1. Instantiate an AlertDialog.Builder with its constructor
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+                // 2. Chain together various setter methods to set the dialog characteristics
+                builder.SetMessage(e.ToString())
+                       .SetTitle("Error");
+
+
+
+                // 3. Get the AlertDialog from create()
+                AlertDialog dialog = builder.Create();
+                dialog.Show();
+            }
+        }
+    }
+}
+
---
+++ AppsProgrammeren_Eindopdracht2/PublicTransport.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Org.Json;
+using Android.Locations;
+using Android.Content;
+using Android.Widget;
+
+namespace AppsProgrammeren_Eindopdracht2
+{
+    class PublicTransport
+    {
+        /*TODO: Er zijn op dit moment twee constanten: De API_KEY, die nodig is om de data op te mogen halen van de Google Maps Directions API,
+                en ten tweede de URL, waarmee de data wordt opgehaald. Hierwordt de API_KEY aan toegevoegd. */
+        private const string API_KEY = "AIzaSyAo6noQYIyE7EPtpBFQNffDGpTYnXT6tSY";
+        private string URL;
+
+        public PublicTransport(string coordinates, string destination) {
+            URL = "https://maps.googleapis.com/maps/api/directions/json?" +
+            "origin=" + coordinates +
+            "&destination=" + destination +
+            "&alternatives=true&mode=transit&key=";
+        }
+
+        /// <summary>
+        /// Gets the public transport data.
+        /// </summary>
+        /// <returns>Public Transport data</returns>
+        private string GetPublicTransportData()
+        {
+            string data = "";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL + API_KEY);
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Gets the transit details.
+        /// </summary>
+        /// <returns>Transit details.</returns>
+        public string[] GetTransitDetails()
+        {
+            List<string> transitSteps = new List<string>();
+            JSONObject geoData = new JSONObject(GetPublicTransportData());
+            string status = geoData.GetString("status");
+
+            if (status.ToString() != "OK")
+            {
+                string[] error = new string[1];
+                error[0] = "No location was found";
+                return error;
+            }
+
+            JSONArray routes = geoData.GetJSONArray("routes");
+            for (int i = 0; i < routes.Length(); i++)
+            {
+                JSONArray legs = routes.GetJSONObject(i).GetJSONArray("legs");
+                JSONArray steps = legs.GetJSONObject(0).GetJSONArray("steps");
+                for (int j = 0; j < steps.Length(); j++)
+                {
+                    if (steps.GetJSONObject(j).GetString("travel_mode") == "TRANSIT")
+                    {
+                        JSONObject transitStep = steps.GetJSONObject(j);
+                        JSONObject transitDetails = transitStep.GetJSONObject("transit_details");
+                        string arrivalTime = transitDetails.GetJSONObject("arrival_time").GetString("text");
+                        string arrivalStop = transitDetails.GetJSONObject("arrival_stop").GetString("name");
+                        string departureTime = transitDetails.GetJSONObject("departure_time").GetString("text");
+                        string departureStop = transitDetails.GetJSONObject("departure_stop").GetString("name");
+                        transitSteps.Add("Departure: " + departureTime + " - " + departureStop + "\n" + 
+                                         "Arrival: " + arrivalTime + " - " + arrivalStop + "\n");
+                    }
+                }
+            }
+            return transitSteps.ToArray();
+        }
+    }
+}
